5926e7a874d89c8e6cb1bf4bdd8ac8ce133a284a
NaTokh <60501810+NaTokh@users.noreply.github.com>
2022-05-31 11:32:17 -0700

    Fixed the PC_SOURCE_OUT to choose the right value for jalr. Last fix (hopefully): need to pause mem for 2 cyckes, flush registers that go from decode to execute

--- Brand_Cond_Gen.sv
+++ Brand_Cond_Gen.sv
@@ -9,12 +9,10 @@ module Brand_Cond_Gen (REG_INPUTA, REG_INPUTB, DR_MEM_OUT, PC_SOURCE_OUT);
     
     // Asynchronous, use always_comb
       
-always_comb begin
-     
+always_comb begin 
      // Look at current option code for program being executed
      case (DR_MEM_OUT[6:0])
-     
-     
+
      7'b1100011: // First case: Branch instruction (Same OP Code, different funct3) or Jalr
         begin
         
@@ -23,42 +21,42 @@ always_comb begin
                 3'b000: begin // Beq (Branch if equal)
                     // Check to see if current rs1 is equal to rs2. If so, set pcSource to 2. Otherwise, set to 0
                     if (REG_INPUTA == REG_INPUTB) begin
-                        PC_SOURCE_OUT = 2'b10; end
+                        PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                 
                 3'b101: begin // Bge (Branch if greater than or equal to)
                     // Check to see if inverse of blt is true. If so, set pcSource to 2. Otherwise, set to 0
                     if (!($signed(REG_INPUTA) < $signed(REG_INPUTB))) begin
-                        PC_SOURCE_OUT = 2'b10; end
+                        PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                 
                 3'b111: begin // Bgeu (Branch if greater than or equal to unsigned)
                     // Check to see if inverse of bltu is true. If so, set pcSource to 2. Otherwise, set to 0
                     if (!(REG_INPUTA < REG_INPUTB)) begin
-                        PC_SOURCE_OUT = 2'b10; end
+                        PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                     
                 3'b100: begin // Blt (Branch if less than)
                     // Check to see if less than is true. If so, set pcSource to 2. Otherwise, set to 0
                     if ($signed(REG_INPUTA) < $signed(REG_INPUTB)) begin
-                           PC_SOURCE_OUT = 2'b10; end
+                           PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                     
                 3'b110: begin // Bltu (Branch if less than unsigned)
                     // Check to see if less than unsigned is true. If so, set pcSource to 2. Otherwise, set to 0
                     if (REG_INPUTA < REG_INPUTB) begin
-                     PC_SOURCE_OUT = 2'b10; end
+                     PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                 
                 3'b001: begin // Bne (Branch if not equal)
                 // Check to see if inverse of equals is true. If so, set pcSource to 2. Otherwise, set to 0
                     if(!(REG_INPUTA == REG_INPUTB)) begin
-                        PC_SOURCE_OUT = 2'b10; end
+                        PC_SOURCE_OUT = 2'b10; end 
                     else begin PC_SOURCE_OUT = 1'b0; end
                 end
                 
@@ -77,13 +75,17 @@ always_comb begin
         
      7'b1101111: begin // Second case: jal (Jump and link) 
         // Set pcSource to 3 
-            PC_SOURCE_OUT = 2'b11;
+            PC_SOURCE_OUT = 2'b11; //check 2'b01???
+     end
+     
+     7'b1100111: begin // Third case: jalr (Jump and link register) 
+        // Set pcSource to 1 
+            PC_SOURCE_OUT = 2'b01; //check 2'b01???
      end
      
      default: begin// Default case: pcSource is set to 0 (PC + 4)
         PC_SOURCE_OUT = 1'b0;
-        end
-        
+        end  
      endcase
      
 end
